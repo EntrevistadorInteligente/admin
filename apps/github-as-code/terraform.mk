41b68109256e41a7ffbcc15686e3e122406524df
Sergio Valverde <svg153@gmail.com>
2024-03-16 02:56:42 +0100

    use makefile targets for ci and cd

--- apps/github-as-code/terraform.mk
+++ apps/github-as-code/terraform.mk
@@ -14,17 +14,22 @@ clean: ## Clean secrets files
 	@-rm -r *.tfstate
 
 .PHONY: init
+# Initialize a new or existing Terraform working directory by creating initial files, loading any remote state, downloading modules, etc.
 init: ## Initialize Terraform
 	terraform init --upgrade
 
 .PHONY: check
 check: ## Check Terraform files
 	terraform fmt -check=true -diff=true
+
 .PHONY: lint
 lint: format validate ## Lint Terraform files
+
 .PHONY: format
+# Checks that all Terraform configuration files adhere to a canonical format
 format: init ## Format Terraform files
 	terraform fmt -write=true
+
 .PHONY: validate
 validate: init ## Validate Terraform files
 	terraform validate
